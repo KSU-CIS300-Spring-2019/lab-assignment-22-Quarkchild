4cca5340c4f346097e518abb3143e2b01eb4f8b0
Michael Crisp <quarkchild@ksu.edu>
2019-03-25 11:40:33 -0500

    done

---
+++ Ksu.Cis300.TrieLibrary/TrieWithNoChildren.cs
@@ -0,0 +1,56 @@
+﻿/* TrieWithNoChildren.cs
+ * Author: Michael Crisp
+ * Date: 25 March 2019
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ksu.Cis300.TrieLibrary
+{
+    /// <summary>
+    /// An implementation of a Trie using the ITrie 
+    /// interface to deal with the instance of it having
+    /// no children.
+    /// </summary>
+    public class TrieWithNoChildren : ITrie
+    {
+        /// <summary>
+        /// Indicates whether the Trie rooted here, contains the empty string.
+        /// </summary>
+        private bool _hasEmpty;
+
+        /// <summary>
+        /// Adds the string to the child, constructing a new one in this case.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public ITrie Add(string s)
+        {
+            if(s == "")
+            {
+                _hasEmpty = true;
+                return this;
+            }
+
+            return new TrieWithOneChild(s, _hasEmpty);
+        }
+
+        
+        /// <summary>
+        /// Looks up the string in the trie.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public bool Contains(string s)
+        {
+            if(s == "")
+            {
+                return _hasEmpty;
+            }
+            return false;
+        }
+    }
+}
---
+++ Ksu.Cis300.TrieLibrary/TrieWithOneChild.cs
@@ -0,0 +1,82 @@
+﻿/* TrieWithOneChild.cs
+ * Author: Michael Crisp
+ * Date: 25 March 2019
+ * 
+ */ 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ksu.Cis300.TrieLibrary
+{
+    class TrieWithOneChild : ITrie
+    {
+        /// <summary>
+        /// Indicates whether the node contains the empty string.
+        /// </summary>
+        private bool _hasEmpty;
+
+        /// <summary>
+        /// The only node of this Trie, an ITrie as it could be either
+        /// implementation.
+        /// </summary>
+        private ITrie _onlyChild;
+
+        /// <summary>
+        /// The label of this child.
+        /// </summary>
+        private char _childLabel;
+
+        public TrieWithOneChild(string s, bool isEmpty)
+        {
+            if(s == "" || s[0] < 'a' || s[0] > 'z')
+            {
+                throw new ArgumentException();
+            }
+
+            _hasEmpty = isEmpty;
+            _childLabel = s[0];
+            _onlyChild = new TrieWithNoChildren().Add(s.Substring(1));
+        }
+        
+        public ITrie Add(string s)
+        {
+            if(s == "")
+            {
+                _hasEmpty = true;
+                return this;
+            }
+
+            else if(s[0] == _childLabel)
+            {
+                _onlyChild = _onlyChild.Add(s.Substring(1));
+                return this;
+            }
+
+            else
+            {
+                return new TrieWithManyChildren(s, _hasEmpty, _childLabel, _onlyChild);
+            }
+        }
+
+        public bool Contains(string s)
+        {
+            if(s == "")
+            {
+                return _hasEmpty;
+            }
+
+            if(s[0] == _childLabel)
+            {
+                return _onlyChild.Contains(s.Substring(1));
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
